b07c33b22e4d44b8971ba9897a0149a97458e1b8
ODA <twbtarai.blackpepper@gmail.com>
2019-12-31 18:10:07 +0900

    add endoiint

--- reply.py
+++ reply.py
@@ -1,6 +1,13 @@
 from flask import Flask, jsonify, request
+from agricolaenvironment.agricola.game import StandardAgricolaGame, TextInterface, play
 import json
+import uuid
+import elasticsearch
 app = Flask(__name__)
+
+LOG_DIR = "./logs"
+ELASTICSEARCH_ENDPOINT = "https://search-agricola-games-l7ju4eul22s6c4n44sltpq4v6m.ap-northeast-1.es.amazonaws.com"
+GAME_INDEX_NAME = "games"
 
 @app.route("/", methods=['GET'])
 def hello():
@@ -13,6 +20,60 @@ def reply():
     result = {
       "Content-Type": "application/json",
       "Answer":{"Text": answer}
+    }
+    # return answer
+    return jsonify(result)
+
+@app.route('/game/<game_id>', methods=['GET'])
+def getGame(game_id=None):
+    client = elasticsearch.Elasticsearch(ELASTICSEARCH_ENDPOINT, use_ssl=False, verify_certs=False)
+    res = client.get(index=GAME_INDEX_NAME, doc_type="_all", id=game_id)
+    return jsonify(res["_source"]["next_input"])
+
+@app.route('/game/<game_id>', methods=['POST'])
+def postGameAction(game_id=None):
+    client = elasticsearch.Elasticsearch(ELASTICSEARCH_ENDPOINT, use_ssl=False, verify_certs=False)
+    res = client.get(index=GAME_INDEX_NAME, doc_type="_all", id=game_id)
+    
+    game = StandardAgricolaGame(4, game_id)
+    ui = TextInterface()
+    next_state, next_input = play(game, ui, None, LOG_DIR, json.dumps(res["_source"]["game"]), step_execution=True, next_choice_output=request.get_data())
+    save_data = {
+        "game": json.loads(next_state),
+        "next_input": json.loads(next_input),
+    }
+    client.index(index=GAME_INDEX_NAME, doc_type='_doc', id=game_id, body=save_data)
+
+    result = {
+      "Content-Type": "application/json",
+      "game_id": game_id,
+    }
+    # return answer
+    return jsonify(result)
+
+@app.route('/game', methods=['POST'])
+def createNewGame():
+
+    # create game and save state
+    game_id = str(uuid.uuid4())
+    game = StandardAgricolaGame(4, game_id)
+    ui = TextInterface()
+    next_state, next_input = play(game, ui, None, LOG_DIR, None, step_execution=True, next_choice_output=None)
+
+    save_data = {
+        "game": json.loads(next_state),
+        "next_input": json.loads(next_input),
+    }
+
+    # save next_state and next_input
+    # TODO use SSL and certs
+    client = elasticsearch.Elasticsearch(ELASTICSEARCH_ENDPOINT, use_ssl=False, verify_certs=False)
+    client.index(index=GAME_INDEX_NAME, doc_type='_doc', id=game_id, body=save_data)
+
+
+    result = {
+      "Content-Type": "application/json",
+      "game_id": game_id,
     }
     # return answer
     return jsonify(result)
